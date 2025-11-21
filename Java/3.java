467c70074733d7010f44b025df46101c367c9200
Shurahtov-PE04 <micharin9114@gmail.com>
2025-11-21 11:05:19 +0300

    Update 3.java

--- Java/3.java
+++ Java/3.java
@@ -1,1 +1,40 @@
+public class InsertionSort {
 
+    // Метод для сортировки массива
+    public static void insertionSort(int[] array) {
+        // Проходим по всем элементам массива, начиная со второго
+        for (int i = 1; i < array.length; i++) {
+            int key = array[i]; // Текущий элемент, который нужно вставить
+            int j = i - 1; // Индекс предыдущего элемента
+
+            // Перемещаем элементы array[0..i-1], которые больше key
+            // на одну позицию вперед
+            while (j >= 0 && array[j] > key) {
+                array[j + 1] = array[j];
+                j = j - 1;
+            }
+            array[j + 1] = key; // Вставляем key на правильное место
+        }
+    }
+
+// Метод для вывода массива
+public static void printArray(int[] array) {
+    for (int value : array) {
+        System.out.print(value + " ");
+    }
+    System.out.println();
+}
+
+    // Основной метод для тестирования
+    public static void main(String[] args) {
+        int[] array = {12, 11, 13, 5, 6};
+
+        System.out.println("Исходный массив:");
+        printArray(array);
+
+        insertionSort(array);
+
+        System.out.println("Отсортированный массив:");
+        printArray(array);
+    }
+}
