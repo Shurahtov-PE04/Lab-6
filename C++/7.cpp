36f8ee6c993239eef4fe90052078e785e0cde41f
Shurahtov-PE04 <micharin9114@gmail.com>
2025-11-21 11:12:27 +0300

    Update 7.cpp

--- C++/7.cpp
+++ C++/7.cpp
@@ -1,1 +1,60 @@
+#include <iostream>
+#include <vector>
+using namespace std;
 
+void heapify(vector<int>& arr, int n, int i) {
+    int largest = i;  // �������������� ���������� ������� ��� ������
+    int left = 2 * i + 1;  // ����� = 2*i + 1
+    int right = 2 * i + 2;  // ������ = 2*i + 2
+
+    // ��������� ���������� �� ����� �������� ������� �������, ��� ������
+    if (left < n && arr[i] < arr[left]) {
+        largest = left;
+    }
+
+    // ��������� ���������� �� ������ �������� ������� �������, ��� ������
+    if (right < n && arr[largest] < arr[right]) {
+        largest = right;
+    }
+
+    // ������ ������, ���� �����
+    if (largest != i) {
+        swap(arr[i], arr[largest]);  // ����
+        // ���������� ��������� heapify � ����������� ���������
+        heapify(arr, n, largest);
+    }
+}
+
+void heapSort(vector<int>& arr) {
+    int n = arr.size();
+    // ���������� max-heap.
+    for (int i = n / 2 - 1; i >= 0; i--) {
+        heapify(arr, n, i);
+    }
+
+    // ���� �� ������ ��������� ��������
+    for (int i = n - 1; i > 0; i--) {
+        swap(arr[0], arr[i]);  // ������ ������ � ��������� ���������
+        heapify(arr, i, 0);
+    }
+}
+
+// ������ �������������:
+int main() {
+    vector<int> arr = { 12, 11, 13, 5, 6, 7 };
+    cout << "�������� ������: ";
+    for (int num : arr) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    heapSort(arr);
+
+    cout << "��������������� ������: ";
+    for (int num : arr) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
