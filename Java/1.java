0addca0c3932e51766e4150f15247c4a2fab8804
Shurahtov-PE04 <micharin9114@gmail.com>
2025-11-21 11:03:39 +0300

    Update 1.java

--- Java/1.java
+++ Java/1.java
@@ -1,1 +1,26 @@
+import java.util.Arrays;
 
+public class SelectionSort {
+    public static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] testArray = {64, 25, 12, 22, 11};
+        System.out.println("Исходный массив: " + Arrays.toString(testArray));
+
+        selectionSort(testArray);
+
+        System.out.println("Отсортированный массив: " + Arrays.toString(testArray));
+    }
+}
